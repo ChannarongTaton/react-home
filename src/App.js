11a831cda6f0e306b5e0b0ec79f86690eea0fbed
Channarong Taton <113801612+ChannarongTaton@users.noreply.github.com>
2022-09-18 19:13:09 +0700

    <LiffComponent/>

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { useNavigate } from 'react-router-dom'
-import { useEffect } from 'react';
-
+// import { useNavigate } from 'react-router-dom'
+// import { useEffect } from 'react';
+import LiffComponents from './Components/LiffComponent'
 function App(props) {
-  const { params } = props
-  const navigate = useNavigate()
+  // const { params } = props
+  // const navigate = useNavigate()
   
-  useEffect(() => {
-    if(params === 'register') {
-      navigate('/register')
-    } else if (params === 'controller') {
-      navigate('/liff-home')
-    } else if (params === 'controller99-1'){
-      navigate('/home2')
-    }
-    // eslint-disable-next-line
-  },[])
+  // useEffect(() => {
+  //   if(params === 'register') {
+  //     return <h1>HELLO {params}</h1>
+  //   } else if (params === 'controller') {
+  //     navigate('/liff-home')
+  //   } else if (params === 'controller99-1'){
+  //     navigate('/home2')
+  //   }
+  //   // eslint-disable-next-line
+  // },[])
   return (
     <div className="App">
-      <div className='pt-6'>
-      <h1 className='App-h1'>หน้าต่างควบคุม</h1>
-      </div>
+      <LiffComponents/>
     </div>
   );  
 }
